cd2589fb4f71dc123427bc2bd679767f95acdde2
able884 <2864824441@qq.com>
2026-04-02 14:00:52 +0800

    fix:换行符为LF

--- consumer/internal/config/config.go
+++ consumer/internal/config/config.go
@@ -4,7 +4,7 @@ import (
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/zrpc"
 
-	constants "richcode.cc/dex/pkg/constrants"
+	constants "richcode.cc/dex/pkg/constants"
 )
 
 var Cfg Config
